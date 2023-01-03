bf47653e4bacc70bfe3a39ae7a56f85d8572bdb4
Ewuru Freeborn <109674539+15sxope@users.noreply.github.com>
2023-01-03 04:36:21 +0100

    update copyright text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer>
-      <p>Copyright Fortune Ewuru ⓒ {year}</p>
+      <p>Copyright  ⓒ Fortune Ewuru {year}</p>
     </footer>
   );
 }
